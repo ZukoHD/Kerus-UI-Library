56e3b7229fa26ec54e8aba55a49ebdd846f30839
dori <69031971+ZukoHD@users.noreply.github.com>
2022-01-18 19:34:00 +1100

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,8 @@
+local lib = loadstring(game:HttpGet("https://pastebin.com/raw/3uTEfEs0",true))()
+
+button = lib:CreateWindow('Example - ???')
+button:CreateButton('Example', function()
+	print('example button works')
+end)
+
+
