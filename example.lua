d99aad205cbd1b3f11be4ace7e43b10de45b3485
dori <69031971+ZukoHD@users.noreply.github.com>
2022-01-19 19:42:39 +1100

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,8 +1,24 @@
+-- Get the actual library
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZukoHD/Kerus-UI-Library/main/library.lua",true))()
 
-Window = lib:CreateWindow('Example Hub')
-button = Window:CreateButton('Example', function()
-	notif = lib:Notify('Example works!')
+-- Create a window and buttons
+Window = lib:CreateWindow('Showcase Hub')
+button1 = Window:CreateButton('Notification', function()
+    -- Define function
+	notif = lib:Notify('Notification from Kerus!')
     wait(4)
     notif:Delete()
 end)
+
+-- Same above for the rest
+button2 = Window:CreateButton('Change Name', function()
+	Window:Edit('Name Changed!')
+    wait(4)
+    Window:Edit('Showcase Hub')
+end)
+button3 = Window:CreateButton('Change Button Name', function()
+	button3:Edit('Name Changed!')
+    wait(4)
+    button3:Edit('Change Button Name')
+end)
+
